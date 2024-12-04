fc0f03269c96e4df6297069009fbbc3d942d1bdd
Xhesas <namnak.xhesas@gmail.com>
2024-12-04 01:27:57 +0100

    added some stuf

--- js/2024.js
+++ js/2024.js
@@ -1,8 +1,31 @@
 async function loadcolors() {
     try {
-        const response = await fetch('colors.json');
+        const response = await fetch('resources/colors.json');
         const colors = await response.json();
+
         setborders(colors);
+
+        const resptwonse = await fetch('resources/songs.json');
+        const list = await resptwonse.json();
+
+        const table = document.getElementById('participants');
+
+        list.filter(item => item.year == 2024).sort((a, b) => a.ro - b.ro).forEach(item => {
+            const row = document.createElement('tr');
+            row.onclick = function() {location.href=`/song.html?name=${encodeURI(item.name)}`};
+            row.innerHTML =`
+                <td class="left">${item.ro}</td>
+                <td>
+                    <div class="thumb">
+                        <div style="background-image:url(/images/${item.thumb})"></div>
+                    </div>
+                </td>
+                <td class="bold">${item.artist}</td>
+                <td>${item.country}</td>
+                <td>${item.name}</td>
+            `;
+            table.appendChild(row);
+        });
     } catch (error) {
         console.error('Error loading file:', error);
     }
@@ -26,3 +49,19 @@ function setborders(list) {
 }
 
 document.addEventListener("load", loadcolors());
+
+function toggletable() {
+    const table = document.getElementById('hideme');
+    const a = document.getElementById('a');
+    const b = document.getElementById('b');
+
+    if (table.classList.contains('hidden')) {
+        table.classList.remove('hidden');
+        a.classList.remove('hidden');
+        b.classList.add('hidden');
+    } else {
+        table.classList.add('hidden');
+        b.classList.remove('hidden');
+        a.classList.add('hidden');
+    }
+}
